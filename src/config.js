3a5b857bc7ab0fa62cf13b8eb00f65a0a1dcf0a6
Cameron DeCoster <cameron.decoster@gmail.com>
2020-07-03 13:09:21 -0600

    remove old files; prep for first deploy

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || "development",
     DATABASE_URL:
         process.env.DATABASE_URL ||
-        "postgresql://et_mod@localhost/expense-tracker",
+        "postgresql://qb_mod@localhost/quiz-builder",
     JWT_SECRET: process.env.JWT_SECRET || "marblerumfingertoast",
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
 };
